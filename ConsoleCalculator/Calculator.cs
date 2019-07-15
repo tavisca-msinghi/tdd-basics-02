2b9313d6a17e49db4694202feb9cffc8c519e3d8
tavisca-msinghi <52082225+tavisca-msinghi@users.noreply.github.com>
2019-07-15 22:38:04 +0530

    Update Calculator.cs

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -3,11 +3,21 @@
 namespace ConsoleCalculator
 {
     public class Calculator
-    {
+    {   
+        private isvalid(char )
 
         public string SendKeyPress(char key)
         {
             // Add your implementation here.
+            
+            if(char.isdigit(key)){
+                if(isvalid(key))
+                int k=char.istodigit(key);
+            
+            }
+            else{
+            
+            }
             throw new NotImplementedException();
         }
     }
